8a5117848ea0d14dcbdeef781e3c297737fa8ced
Minh Le <mle003@plattsburgh.edu>
2021-09-25 16:53:21 -0400

    background images

--- my-website/src/containers/Home.js
+++ my-website/src/containers/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div id='home' className='h-screen mt-10' style={{ backgroundImage: "url('images/background2.PNG')" }}>
+    <div id='home' className='h-screen mt-10'>
       <div style={{ background: 'rgba(0, 0, 0, 0.5)' }} className='h-full background text-white text-center pt-60 px-56'>
         <h1 className='text-6xl mb-3 text_shadows'>Minh Le</h1>
         <h2 className='text-4xl mb-3 w3-animate-bottom'>A Vietnamese Software Engineer. Interested in Web, Mobile App and Game development</h2>
